119f7a74ae077e0c0f53d732a3cc9d0fe5526db0
Vivaldy Andhira <dididlav@gmail.com>
2020-08-24 11:55:14 +0700

    added HomeActivity

---
+++ app/src/main/java/com/example/enigma_bank/domains/login/screen/LoginFragment.kt
@@ -0,0 +1,20 @@
+package com.example.enigma_bank.domains.login.screen
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.enigma_bank.R
+
+class LoginFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_login, container, false)
+    }
+
+}
